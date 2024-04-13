5597934d32e8237f8750852ff7f358a46799d583
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-04-13 14:56:06 -0500

    tyescript configuration added

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function askGPT() {
+    console.log("Ask GPT");
+    return "Ask GPT";
+}
+exports.default = askGPT;
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 function askGPT() {
   console.log("Ask GPT");
+  return "Ask GPT";
 }
 
 export default askGPT;
